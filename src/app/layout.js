7a0ba20a7d94767998356d72ee8e9ef0afb287d1
Umesh Joshi <uj19343@gmail.com>
2025-08-05 19:32:47 +0545

    silder error in about page fixing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../component/Navbar";
 import Footer from "@/component/Footer";
--- src/component/projectImage/ProjectImage.jsx
+++ src/component/projectImage/ProjectImage.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
-// import { baseUrl } from "./config";
+import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
+import styles from "./project.module.css"; // CSS Module import
 
 function LazyLoad() {
   const settings = {
@@ -12,29 +14,38 @@ function LazyLoad() {
     slidesToShow: 1,
     slidesToScroll: 1,
     initialSlide: 2,
+    dotsClass: `slick-dots ${styles.dots}`,
+    prevArrow: <div className={styles.arrow} />,
+    nextArrow: <div className={styles.arrow} />,
   };
 
-  const imageLink = [
-    "/project/1.png",
-    "/project/2.png",
-    "/project/3.png",
-    "/project/4.png",
+  const images = [
+    { src: "/project/1.png", alt: "Project 1" },
+    { src: "/project/2.png", alt: "Project 2" },
+    { src: "/project/3.png", alt: "Project 3" },
+    { src: "/project/4.png", alt: "Project 4" },
   ];
+
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        <div>
-          <Image src="/project/1.png" width={100} height={100} alt="project" />
-        </div>
-        <div>
-          <Image src="/project/1.png" width={100} height={100} alt="project" />
-        </div>
-        <div>
-          <Image src="/project/1.png" width={100} height={100} alt="project" />
-        </div>
-        <div>
-          <Image src="/project/1.png" width={100} height={100} alt="project" />
-        </div>
+        {images.map((image, index) => (
+          <div key={index} className="px-2">
+            {" "}
+            {/* Add some padding between slides */}
+            <div className="relative w-full h-96">
+              {" "}
+              {/* Set a fixed height for all slides */}
+              <Image
+                src={image.src}
+                alt={image.alt}
+                fill
+                className="object-cover border border-black rounded-lg"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </div>
+          </div>
+        ))}
       </Slider>
     </div>
   );
