136be8e4dc84bf64e0046f8e8efc9c73b64da684
Umesh Joshi <uj19343@gmail.com>
2025-08-12 21:37:55 +0545

    working on project

--- src/app/about-me/page.jsx
+++ src/app/about-me/page.jsx
@@ -7,7 +7,6 @@ import React from "react";
 import Image from "next/image";
 
 const AboutMe = () => {
-  const project = [1, 2];
   return (
     <div className="text-black w-full relative top-20 ">
       {/* <Description /> */}
@@ -36,25 +35,6 @@ const AboutMe = () => {
           <h1 className="text-6xl font-extrabold text-center">My Skill</h1>
         </div>
         <SkillDetails />
-      </div>
-
-      <div className="w-full">
-        <div className="w-full text-4xl font-extrabold text-center py-10">
-          <h1>My Project</h1>
-        </div>
-        <div className="p-10 text-black ">
-          <div className="flex gap-30 flex-wrap justify-center">
-            {/* <SimpleSlider images={images} /> */}
-            {project.map((value, index) => {
-              return <ProjectCard key={index} values={value} />;
-            })}
-          </div>
-        </div>
-        <div className="flex p-5 justify-center relative top-0 ">
-          <button className="px-5 py-3 text-lg cursor-pointer rounded-lg  bg-black text-white hover:bg-white hover:text-black hover:border hover:font-bold">
-            See more Project
-          </button>
-        </div>
       </div>
     </div>
   );
--- src/app/my-project/page.jsx
+++ src/app/my-project/page.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import ProjectCard from "../../component/project-details/ProjectCard";
 const MyProject = () => {
-  return <div className="">MyProject</div>;
+  const project = [1, 2];
+  return (
+    <div className="">
+      <div className="w-full">
+        <div className="w-full text-4xl font-extrabold text-center py-10">
+          <h1>My Project</h1>
+        </div>
+        <div className="p-10 text-black ">
+          <div className="flex gap-30 flex-wrap justify-center">
+            <SimpleSlider images={images} />
+            {project.map((value, index) => {
+              return <ProjectCard key={index} values={value} />;
+            })}
+          </div>
+        </div>
+        <div className="flex p-5 justify-center relative top-0 ">
+          <button className="px-5 py-3 text-lg cursor-pointer rounded-lg  bg-black text-white hover:bg-white hover:text-black hover:border hover:font-bold">
+            See more Project
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyProject;
