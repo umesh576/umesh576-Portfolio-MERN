28510fee65e88e339798e324ec529820568bfc51
Umesh Joshi <uj19343@gmail.com>
2025-08-20 21:48:29 +0545

    chnages on button

--- src/component/project-details/ProjectThird.jsx
+++ src/component/project-details/ProjectThird.jsx
@@ -19,7 +19,7 @@ const ProjetCardThird = ({ projectDetail }) => {
           </h1>
         </div>
         <div className="flex justify-center gap-2 p-2">
-          <button className="px-3 py-2 bg-black text-white text-lg rounded-lg hover:bg-gray-300 cursor-pointer  hover:text-black hover:animate-ping ">
+          <button className="px-3 py-2 bg-black text-white text-lg rounded-lg hover:bg-gray-300 cursor-pointer  hover:text-black">
             See demo
           </button>
           <button className="px-3 py-2 bg-gray-200 text-black text-lg rounded-lg hover:bg-gray-500 cursor-pointer  hover:text-black hover:animate-pulse">
