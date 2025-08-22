09a6e740bde0dab0d6cfea685423f4b59b3e3c91
Umesh Joshi <uj19343@gmail.com>
2025-08-22 17:23:35 +0545

    adding new feature

--- src/app/services-me/page.jsx
+++ src/app/services-me/page.jsx
@@ -1,7 +1,33 @@
+import ServiceCard from "@/component/servicesSection/ServiceCard";
 import React from "react";
 
 const ServicePage = () => {
-  return <div>ServicePage</div>;
+  const services = [
+    {
+      name: "App Development",
+      description:
+        "I am Full satck app developer. I am using Reactnative in app developmet.",
+    },
+    {
+      name: "Website Development",
+      description:
+        "I am Full satck web developer. I am using next and node in web developmet.",
+    },
+  ];
+  return (
+    <div>
+      <div>
+        <div>
+          {/* <h1>Services</h1> */}
+          <div>
+            {services.map((service, index) => {
+              return <ServiceCard service={service} key={index} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServicePage;
